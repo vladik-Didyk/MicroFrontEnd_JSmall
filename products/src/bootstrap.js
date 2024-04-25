137e9d9081cc8da7262eea9426a31c542d227b3f
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2024-04-25 08:02:48 -0400

    24. Sub-App Execution Context

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -8,3 +8,15 @@ for (let i = 0; i < 5; i++) {
 }
 
 document.querySelector('#dev-products').innerHTML = products;
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which DEFNITELY has an element with an id of 'dev-products
+// We want to immediately render our app into that element
+
+// Context/Situation #2
+// We are running this file in develpment or production
+// through the CONTAINER app
+// NO GUARANTEE that an element with an id of 'dev-products' exists
+// WE DO NOT WANT try to immediately render the app
